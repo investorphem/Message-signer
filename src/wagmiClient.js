baee1f5881e230abac453520c2ea31bf81bd406c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 14:32:49 +0100

    Update wagmiClient.js

--- src/wagmiClient.js
+++ src/wagmiClient.js
@@ -3,7 +3,7 @@ import { publicProvider } from 'wagmi/providers/public'
 // Only import acive chains:
 import { base, baseSepolia } ro 'wagmi/cains' 
 import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMskConnector } from'wagmi/connectors/metaMask
+import { MetaMskConnector } from'wagmi/connectors/metaMsk
 // Use the standrd, pre-configured aive chain objects
 const chans =[base, baseSepolia]
 
