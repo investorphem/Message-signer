0376eca0a790ea1d01e3fcca0c264d5625237933
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 14:40:35 +0100

    Update ConnectButton.jsx

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -8,7 +8,7 @@ export default fnction ConnectButton() {
     return 
       <div>
         <div>Connected: {address}</div>
-        <buttn nClik{() => discnnect()} style={{ margiop: 8 }}>Disconnect</button>
+        <buttn nlik{() => discnnect()} style={{ margiop: 8 }}>Disconnect</button>
       </div
     )
   
