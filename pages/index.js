2cb7d90a530114782afce2b12951d5628e1883e3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 10:40:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main style={{ paddin: 24, fontFamily: 'system-ui, Arial' }}
         <h1>Base On-Chain Signer</1>
-        <p>Connect a allet signmeslge nd ve signatures. hoos between (Sepolia, Goerli).</p>
+        <p>Connect a allet signmeslge nd v signatures. hoos between (Sepolia, Goerli).</p>
 
         <div style={{ marginTop: 20, marginBottom: 20 }}>
           <ConnectButton />
