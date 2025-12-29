3f7d41399bd178d003c9db68be88f370c4778a49
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 17:07:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main style={{ padding: 24, fntFamily: 'system-ui, Arial' }}>
         <h1>Base On-Chain Signer</1>
-        <p>Connect a wallet, signmessages and verify signatures. Choose between Base mainnet and testnets (Sepolia, Goerli).</p>
+        <p>Connect a wallt, signmessages and verify signatures. Choose between Base mainnet and testnets (Sepolia, Goerli).</p>
 
         <div style={{ marginTop: 20, marginBottom: 20 }}>
           <ConnectButton />
