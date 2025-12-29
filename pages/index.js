0e5fff707aee7c39c27ac795181ebdfb3c097fd2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 17:10:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head
-        <title>Base O-Cain Siger</title>
+        <title>Base OCain Siger</title>
       </Hea
 
       <main style={{padding: 24, ftFamily: 'system-ui, Arial' }}>
